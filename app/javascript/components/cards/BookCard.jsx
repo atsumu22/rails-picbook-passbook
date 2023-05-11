1a85861df5083fb7079fb7d2f5ff7e0ee035c3d9
atsumu22 <w.atsumu27@gmail.com>
2023-05-11 13:28:46 +0900

    created bookcard component

--- app/javascript/components/cards/BookCard.jsx
+++ app/javascript/components/cards/BookCard.jsx
@@ -1,67 +1,29 @@
-import axios from 'axios';
-import React, { useState } from 'react';
+import React from 'react';
+import styled from "styled-components";
 
 const BookCard = (props) => {
-  const { result } = props;
-  const [ book, setBook ] = useState();
-
-  const fetchedData = axios.get(`https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?format=json&isbn=${result}&applicationId=1018371623494845154`).then((res) => {
-    if (res.data.Items.length === 0) {
-      fetchingFromOpenBD(result);
-    } else {
-      setBook({
-        title: res.data.Items[0].Item.title,
-        author: res.data.Items[0].Item.author,
-        publisher: res.data.Items[0].Item.publisherName,
-        imageUrl: res.data.Items[0].Item.mediumImageUrl,
-        price: res.data.Items[0].Item.itemPrice
-      })
-    };
-    // console.log(res.data.Items[0].Item);
-  });
-
-  const fetchingFromOpenBD = (result) => {
-    axios.get(`https://api.openbd.jp/v1/get?isbn=${result}`).then((res) => {
-      if (res.data[0] === null) {
-        // console.log("APIが存在しない")
-        setBook(null)
-      } else {
-        if (res.data[0].onix.ProductSupply.SupplyDetail.Price) {
-          console.log(res.data[0].onix.ProductSupply.SupplyDetail.Price[0].PriceAmount);
-          setBook({
-            title: res.data[0].summary.title,
-            author: res.data[0].summary.author,
-            publisher: res.data[0].summary.publisher,
-            imageUrl: res.data[0].summary.cover,
-            price: res.data[0].onix.ProductSupply.SupplyDetail.Price[0].PriceAmount
-            // priceのプロパティに演算子を設定。Price情報が存在するなら、価格詳細をプロパティ値としてセット、存在しない場合は"no-price"とかfalseを返すものをセット。あとで、ここの情報を取り出しやすいようにする。
-          })
-        } else {
-          console.log("価格情報が存在しません");
-          // 上の実装ができれば、ここのブロックは削除可能。
-        }
-      }
-    })
-  };
+  const { book } = props;
 
   return (
-    <div>
-      { book ?  <div className="bookcard">
-        <div className="bookimg">
-          <img src={book.imageUrl} alt="book-image" />
-        </div>
-        <div className="bookinfo">
-          <p className="bookinfo__title">{book.title}</p>
-          <p className="bookinfo__author">{book.author}</p>
-          <p className="bookinfo__publisher">{book.publisher}</p>
-          <div className="bookinfo__buttons">
-          <button className="btn btn-primary">Detail</button>
-          </div>
+    <div className="bookcard">
+      <div className="bookimg">
+        <img src={book.imageUrl} alt="book-image" />
+      </div>
+      <div className="bookinfo">
+        <p className="bookinfo__title">{book.title}</p>
+        <p className="bookinfo__author">{book.author}</p>
+        <p className="bookinfo__publisher">{book.publisher}</p>
+        <div className="bookinfo__buttons">
+        <button className="btn btn-primary">Detail</button>
         </div>
       </div>
-      : <div className="bookcard"><h2>本情報が取得できませんでした。キーワード検索をご利用ください。</h2></div> }
     </div>
   );
 };
+
+const SBookCard = styled.div`
+
+`;
+
 
 export default BookCard;
